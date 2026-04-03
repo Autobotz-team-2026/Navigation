c332a14616b132768b1ec55ae7ee932c7f39fe95
jpedroevg <joaopedroevangelista63@gmail.com>
2026-04-03 15:15:05 -0300

    Scara states

--- src/scara_controller/scara_controller/scara.py
+++ src/scara_controller/scara_controller/scara.py
@@ -3,6 +3,7 @@ import rclpy
 from rclpy.node import Node
 from std_msgs.msg import Float32MultiArray
 import math
+from std_msgs.msg import String
 from std_msgs.msg import Float64
 
 class JointPID:
@@ -41,25 +42,20 @@ class ScaraControl(Node):
     def __init__(self):
         super().__init__('cmd_pub')
 
-        #Manipulator datas:
-        self.l1 = 0.425
-        self.l2 = 0.35
+        #Manipulator lengths.
+        self.l1 = 0.425     #First arm
+        self.l2 = 0.35      #Second arm
 
-        
-    
+        #Initials variables.
+        self.goal = [0.0, 0.0]                   #Final goal to manipulator.
+        self.theta1 = 0.0                        #First joint rotation.
+        self.theta2 = 0.0                        #Second joint rotation.
+        self.manipulator_state = "Stand By"      #Manipulator's state.
 
-        #Initials variables
-        self.goal = [0.32, 0.0]
-        self.theta1 = 0.0
-        self.theta2 = 0.0
-        self.cords_received = True
-
-
-        self.pid1 = JointPID(500, 0, 0, self.theta1)
-        self.pid2 = JointPID(500, 0, 0, self.theta2)
         #Publishers and Subscribers
-        self.goalSub = self.create_subscription(Float32MultiArray, '/goal_pose', self.goal_pose_setter, 10)
-        self.arm1Pub = self.create_publisher(Float64, '/scara_arm_joint1/cmd_pos', 10)
+        self.stateCmdSub = self.create_subscription(String, '/scara/command', self.state_setter, 10) #Take the string from "/scara/command" and set the manipulator's state: "Retract", "Pick Block" or "Place Block".
+        self.goalSub = self.create_subscription(Float32MultiArray, '/goal_pose', self.goal_pose_setter, 10) #Take the goal from "/goal_pose" and set the variables.
+        self.arm1Pub = self.create_publisher(Float64, '/scara_arm_joint1/cmd_pos', 10) 
         self.arm2Pub = self.create_publisher(Float64, '/scara_arm_joint2/cmd_pos', 10)
         self.heightPub = self.create_publisher(Float64, '/scara_height_joint0/cmd_pos', 10)
         self.clawRotPub = self.create_publisher(Float64, '/scara_claw_rotation_joint0/cmd_pos', 10)
@@ -67,36 +63,41 @@ class ScaraControl(Node):
         self.imusSub = self.create_subscription(Float32MultiArray, '/imus_yaw', self.pidCalc, 10)
 
     def pidCalc(self, msg):
-        self.pid1.goal = self.theta1
-        self.pid2.goal = self.theta2
-        now_sec = self.get_clock().now().nanoseconds / 1e9
-        
-        current_theta1 = msg.data[1]
-        current_theta2 = msg.data[2]
+        if self.manipulator_state != "Stand By":
+            self.pid1.goal = self.theta1
+            self.pid2.goal = self.theta2
+            now_sec = self.get_clock().now().nanoseconds / 1e9
+            
+            current_theta1 = msg.data[1]
+            current_theta2 = msg.data[2]
 
-        v_control1 = self.pid1.calculate(current_theta1, now_sec)
-        v_control2 = self.pid2.calculate(current_theta2, now_sec)
+            v_control1 = self.pid1.calculate(current_theta1, now_sec)
+            v_control2 = self.pid2.calculate(current_theta2, now_sec)
 
-        cmd1_msg = Float64()
-        cmd1_msg.data = v_control1
-        cmd2_msg = Float64()
-        cmd2_msg.data = v_control2
-        self.arm1Pub.publish(cmd1_msg)
-        self.arm2Pub.publish(cmd2_msg)
-        
-        
-
+            cmd1_msg = Float64()
+            cmd1_msg.data = v_control1
+            cmd2_msg = Float64()
+            cmd2_msg.data = v_control2
+            self.arm1Pub.publish(cmd1_msg)
+            self.arm2Pub.publish(cmd2_msg)
+            
     def goal_pose_setter(self, msg):
-        if len(msg.data) >= 2:
+        if self.manipulator_state == "Pick Block":
             self.goal[0] = msg.data[0]
             self.goal[1] = msg.data[1]
-            self.cords_received = True
             self.get_logger().info(f"Coordenadas recebidas: ({self.goal[0], self.goal[1]})")
-        else:
-            self.get_logger().info("Coordenadas não foram recebidas devidamente.")
 
+        elif self.manipulator_state == "Retract Arm":
+            self.goal[0] = -0.775
+            self.goal[1] = 0
+            self.get_logger().info(f"Retraindo braço...")
+
+        elif self.manipulator_state == "Place Block":
+            self.goal[0] = 0.775
+            self.goal[1] = 0
+            
     def thetasCalc(self):
-        if self.cords_received:
+        if self.manipulator_state != "Stand By":
             self.get_logger().info("Iniciando o cálculo da cinemática inversa...")
             #Calculating theta2:
             cos_theta2 = (self.goal[0]**2 + self.goal[1]**2 - self.l1**2 - self.l2**2)/(2*self.l1*self.l2)
@@ -106,6 +107,18 @@ class ScaraControl(Node):
             #Calculating theta1:
             self.theta1 = math.atan2(self.goal[1], self.goal[0]) - math.atan2(self.l2*math.sin(self.theta2), self.l1 + self.l2*cos_theta2)
 
+    def state_setter(self, msg):
+        if msg.data == "Retract":
+            self.manipulator_state = "Retract Arm"
+            self.get_logger().info("Retraindo")
+        elif msg.data == "Pick Block":
+            self.manipulator_state = "Pick Block"
+            self.get_logger().info("Pegando bloco")
+
+        elif msg.data == "Place Block:":
+            self.manipulator_state = "Place Block"  
+            self.get_logger().info("Colocando bloco.")
+      
 
 def main(args = None):
     rclpy.init(args = args)
